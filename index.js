fca46fcfbc81980764c8241d74d770d72900b5d4
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-12-26 22:15:12 +0200

    prettier

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-
-const argv = require('yargs').argv;
+const argv = require("yargs").argv;
 const contacts = require("./contacts");
 
-const invokeAction = async ({action, id, name, email, phone}) => {
-    switch(action) {
-        case "list":
-            const allContacts = await contacts.listContacts();
-            return console.table(allContacts);
-        case "get":
-            const contactByID = await contacts.getContactById(id);
-            return console.table(contactByID);
-        case "add":
-            const newBook = await contacts.addContact({name, email, phone});
-            return console.table(newBook);
-       
-        case "remove":
-            const deleteContact = await contacts.removeContact(id);
-            return console.table(deleteContact);
-        default:
-            console.warn('\x1B[31m Unknown action type!');   
-    }
-}
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+    case "get":
+      const contactByID = await contacts.getContactById(id);
+      return console.table(contactByID);
+    case "add":
+      const newBook = await contacts.addContact({ name, email, phone });
+      return console.table(newBook);
+
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      return console.table(deleteContact);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
 
 invokeAction(argv);
