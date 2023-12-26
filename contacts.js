55b9ad69b482f9b452fbb5e2538ba76db4df2fe4
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-12-26 18:01:04 +0200

    mentors comments

--- contacts.js
+++ contacts.js
@@ -16,15 +16,15 @@ return JSON.parse(data);
       const contacts = await listContacts();
       const newContact = {
           id: nanoid(),
-       ...data
+          ...data
         }
         contacts.push(newContact)
         await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 1))
         return newContact
     }
 
-    const removeContact = async (contactId) => {
-      const contacts = await listContacts()
+  const removeContact = async (contactId) => {
+  const contacts = await listContacts()
   const index = contacts.findIndex (contact => contact.id === contactId)
   if (index === -1) { return null}
   const [result] = contacts.splice(index, 1)
--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 
 const argv = require('yargs').argv;
-
 const contacts = require("./contacts");
 
 const invokeAction = async ({action, id, name, email, phone}) => {
     switch(action) {
         case "list":
             const allContacts = await contacts.listContacts();
-            return console.log(allContacts);
+            return console.table(allContacts);
         case "get":
             const contactByID = await contacts.getContactById(id);
-            return console.log(contactByID);
+            return console.table(contactByID);
         case "add":
             const newBook = await contacts.addContact({name, email, phone});
-            return console.log(newBook);
+            return console.table(newBook);
        
         case "remove":
             const deleteContact = await contacts.removeContact(id);
-            return console.log(deleteContact);
+            return console.table(deleteContact);
         default:
             console.warn('\x1B[31m Unknown action type!');   
     }
 }
 
 invokeAction(argv);
-
-
-// invokeAction({action: "list"});
-// invokeAction({action: "get", id: "qdggE76Jtbfd9eWJHrssH"});
-// invokeAction({action: "remove", id: "tqLMsgUxDsHdMO6SWXJOd"});
-// invokeAction({action: "add", name: "Kateryna Mala", email: "mala1234@gmail.com", phone:"(222) 000-1111"});
-
-
